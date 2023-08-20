b22bb52427dcc84f3693147145909c7cb790d0ea
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-08-20 23:38:21 +0100

    implement exercise 005

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,35 @@
 package com.techreturners.exercise005;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Exercise005 {
 
+    private final Map<Character, Boolean> alphabetMap = new HashMap<>();
+
+    public boolean checkIfLettersArePresent() {
+        for (boolean letterPresent : alphabetMap.values()) {
+            if (!letterPresent) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        for (char c = 'a'; c <= 'z'; c++) {
+            alphabetMap.put(c, false);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                c = Character.toLowerCase(c);
+                if (alphabetMap.containsKey(c)) {
+                    alphabetMap.put(c, true);
+                }
+            }
+        }
+
+        return checkIfLettersArePresent();
     }
 
 }
