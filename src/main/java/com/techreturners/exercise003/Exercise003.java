a69cf15d35ebfbde17ddad73596d83a111b7f766
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-08-20 15:39:34 +0100

    Implement exercise 003 - iceCreamFlavours and refactor getIceCreamCode

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -2,20 +2,27 @@ package com.techreturners.exercise003;
 
 public class Exercise003 {
 
+    private String pistachio = "Pistachio";
+    private String raspberryRipple = "Raspberry Ripple";
+    private String vanilla = "Vanilla";
+    private String mintChocolateChip = "Mint Chocolate Chip";
+    private String chocolate = "Chocolate";
+    private String mangoSorbet = "Mango Sorbet";
+
     int getIceCreamCode(String iceCreamFlavour) {
         int iceCreamCode = 0;
 
-        if (iceCreamFlavour == "Pistachio") {
+        if (iceCreamFlavour == pistachio) {
             iceCreamCode = 0;
-        } else if (iceCreamFlavour == "Raspberry Ripple") {
+        } else if (iceCreamFlavour == raspberryRipple) {
             iceCreamCode = 1;
-        } else if (iceCreamFlavour == "Vanilla") {
+        } else if (iceCreamFlavour == vanilla) {
             iceCreamCode = 2;
-        } else if (iceCreamFlavour == "Mint Chocolate Chip") {
+        } else if (iceCreamFlavour == mintChocolateChip) {
             iceCreamCode = 3;
-        } else if (iceCreamFlavour == "Chocolate") {
+        } else if (iceCreamFlavour == chocolate) {
             iceCreamCode = 4;
-        } else {
+        } else if (iceCreamFlavour == mangoSorbet) {
             iceCreamCode = 5;
         }
 
@@ -23,7 +30,8 @@ public class Exercise003 {
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        String[] currentFlavours = { pistachio, raspberryRipple, vanilla, mintChocolateChip, chocolate, mangoSorbet };
+        return currentFlavours;
     }
 
 }
